983c839e94e07dc4307e6059cbc4b1fa5a00753b
GitHub Action <action@github.com>
2020-12-28 13:03:27 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kamitme/apitransfromplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kamitme.apitransfromplugin.services
+
+import com.github.kamitme.apitransfromplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kamitme/apitransfromplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kamitme/apitransfromplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kamitme.apitransfromplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kamitme.apitransfromplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
